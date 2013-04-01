912085ca4b27db585af4e682ed784f5bfc509650
Ding <dingyi@me.com>
2013-04-01 11:49:13 +0800

    updated get app icons workflow

--- Get App Icons/get.py
+++ Get App Icons/get.py
@@ -10,14 +10,14 @@ import urllib,os,json
 
 def getIcons(appid='578553766'):
     output='~/Desktop/'
-    folder=os.path.expanduser(output+appid+'/')
+    
+    app=json.loads(urllib.urlopen('http://itunes.apple.com/lookup?id='+appid).read())['results'][0]
+
+    folder=os.path.expanduser(output+app['trackName']+'/')
     
     if not os.path.exists(folder):
         os.mkdir(folder)
 
-    app=json.loads(urllib.urlopen('http://itunes.apple.com/lookup?id='+appid).read())['results'][0]
-
-    name=app['artistName']
 
     icon512=app['artworkUrl512']
 
@@ -31,4 +31,4 @@ def getIcons(appid='578553766'):
     output.write(urllib.urlopen(icon175).read())
     output.close()
 
-    return app['trackId']
+    return app['trackName']
